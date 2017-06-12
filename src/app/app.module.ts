9b6aae8b3cb8cfc58245648d98cd606cd458291e
Gajendra Singh <gajendra.singh@digitaslbi.com>
2017-06-12 21:47:29 +0530

    Exercise skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
