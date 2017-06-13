55c52b6fbcb23bb5a841c29196b4dfc7bcc2fcb7
Gajendra Singh <gajendra.singh@digitaslbi.com>
2017-06-13 16:43:47 +0530

    Structure Compleated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SearchComponent } from './search/search.component';
+import { CartComponent } from './cart/cart.component';
+import { FooterComponent } from './footer/footer.component';
+import { CatlogComponent } from './catlog/catlog.component';
+import { ContentComponent } from './content/content.component';
+import { ProductFilterComponent } from './product-filter/product-filter.component';
+import { ProductListingComponent } from './product-listing/product-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    SidebarComponent
+    SidebarComponent,
+    SearchComponent,
+    CartComponent,
+    FooterComponent,
+    CatlogComponent,
+    ContentComponent,
+    ProductFilterComponent,
+    ProductListingComponent
   ],
   imports: [
     BrowserModule
